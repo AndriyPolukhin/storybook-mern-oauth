bb14e6330f693c3e57a41da861ed0b7c30df2d9c
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-07-08 22:46:37 +0300

    routes are set up, preleminary views in place

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 // * Dependencies
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const connectDB = require('./config/db');
 
 // * Load Config
 dotenv.config({ path: './config/config.env' });
 
+// * Passport config
+require('./config/passport')(passport);
+
+// * Connect to the database
+connectDB();
+
 // * Create the app object
 const app = express();
 
+// * Setup the logger for the requests
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// * Set the template engine
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// * Session
+app.use(
+  session({
+    secret: 'anastasia',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// * Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// * Static folder for public resources
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ** Link the filtes
+// * Routes
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+
 // * Get the port from the config file
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // * Init the server
 app.listen(
